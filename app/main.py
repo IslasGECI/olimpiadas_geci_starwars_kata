e027ff84bf6fabe0cc839a43ccc5d4772338559c
maritza <maritza.bello@islas.org.mx>
2021-08-04 12:15:29 -0700

    ♻️ Refactor code

--- app/main.py
+++ app/main.py
@@ -9,19 +9,8 @@ def read_root():
     return {"msg": "Hello World from GECI!"}
 
 
-@app.get("/api/people/1/")
-def people_1():
-    r = requests.get('https://swapi.dev/api/people/1/')
-    return r.json()
-
-
-@app.get("/api/planets/3/")
-def planets_3():
-    r = requests.get('https://swapi.dev/api/planets/3/')
-    return r.json()
-
-
-@app.get("/api/starships/9/")
-def starships_9():
-    r = requests.get('https://swapi.dev/api/starships/9/')
+@app.get("/api/{item}/{number}/")
+def people_1(item,number):
+    url=f"https://swapi.dev/api/{item}/{number}/"
+    r = requests.get(url)
     return r.json()
