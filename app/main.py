a1179edd7ee31e292a6b602b888d6a3a4f1dd37b
maritza <maritza.bello@islas.org.mx>
2021-08-04 11:56:30 -0700

    🔥 remove code

--- app/main.py
+++ app/main.py
@@ -15,15 +15,13 @@ def people_1():
     return r.json()
 
 
-@app.get("/api/people/2/")
-def people_2():
-    r = requests.get('https://swapi.dev/api/people/2/')
-    return r.json()
-
-
-
 @app.get("/api/planets/3/")
 def planets_3():
     r = requests.get('https://swapi.dev/api/planets/3/')
     return r.json()
 
+
+@app.get("/api/starships/9/")
+def starships_9():
+    r = requests.get('https://swapi.dev/api/starships/9/')
+    return r.json()
